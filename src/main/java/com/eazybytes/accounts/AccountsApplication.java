f482f67f5dd1a5b5acbfad7e83a4e5e01d6dd15b
unknown <nkibi53@gmail.com>
2026-01-26 12:02:08 +0300

    started implementing docs

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,38 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Eazybank accounts microservice REST API Docs",
+				contact = @Contact(
+						name = "Raphael Ndonga",
+						url = "https://raphaelndonga.com",
+						email = "nkibi53@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				),
+				version = "v1"
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
